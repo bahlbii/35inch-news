ab543e20bcd04a34b967fbdb5f20f866b2750eba
bahlbii <bahlbibg@gmail.com>
2022-11-18 00:55:45 +0300

    landing, nav, style added

---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import Navbar from './Navbar';
+
+const Landing = () => {
+    
+    return (
+        <div>
+            <Navbar/>
+        </div>
+       
+    )
+}
+
+export default Landing;
