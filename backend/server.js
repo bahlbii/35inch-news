9a95249c4fac3cbc88b4d51a52abf9a595fe5e31
bahlbii <bahlbibg@gmail.com>
2022-11-18 15:21:57 +0300

    editing news left

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,6 @@ const db = require('./elephantsql'); //for elephant
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "client/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", 'index.html'));
-})
-
 //POST ROUTE to register user
 app.post("/api/register", async (req, res) => {
 
